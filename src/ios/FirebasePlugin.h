650e842c7eebf8f6b5f454893f27121e5788de6e
Andrii Forkaliuk <aforkalyuk@gmail.com>
2023-01-17 13:49:42 +0200

    refactoring for the latest firebase pods version

--- src/ios/FirebasePlugin.h
+++ src/ios/FirebasePlugin.h
@@ -36,7 +36,7 @@
 - (void)updateUserPassword:(CDVInvokedUrlCommand*)command;
 - (void)sendUserPasswordResetEmail:(CDVInvokedUrlCommand*)command;
 - (void)deleteUser:(CDVInvokedUrlCommand*)command;
-- (void)createiAndLinkUserWithEmailAndPassword:(CDVInvokedUrlCommand*)command;
+- (void)createAndLinkUserWithEmailAndPassword:(CDVInvokedUrlCommand*)command;
 - (void)reauthenticateWithPassword:(CDVInvokedUrlCommand*)command;
 - (void)transferUserToKeychain:(CDVInvokedUrlCommand*)command;
 
@@ -48,14 +48,18 @@
 - (NSString *)hexadecimalStringFromData:(NSData *)data;
 - (void)grantPermission:(CDVInvokedUrlCommand*)command;
 - (void)hasPermission:(CDVInvokedUrlCommand*)command;
+- (void)grantCriticalPermission:(CDVInvokedUrlCommand*)command;
+- (void)hasCriticalPermission:(CDVInvokedUrlCommand*)command;
 - (void)setBadgeNumber:(CDVInvokedUrlCommand*)command;
 - (void)getBadgeNumber:(CDVInvokedUrlCommand*)command;
 - (void)subscribe:(CDVInvokedUrlCommand*)command;
 - (void)unsubscribe:(CDVInvokedUrlCommand*)command;
 - (void)unregister:(CDVInvokedUrlCommand*)command;
+- (void)onOpenSettings:(CDVInvokedUrlCommand*)command;
 - (void)onMessageReceived:(CDVInvokedUrlCommand*)command;
 - (void)onTokenRefresh:(CDVInvokedUrlCommand*)command;
 - (void)onApnsTokenReceived:(CDVInvokedUrlCommand *)command;
+- (void)sendOpenNotificationSettings;
 - (void)sendNotification:(NSDictionary*)userInfo;
 - (void)sendToken:(NSString*)token;
 - (void)sendApnsToken:(NSString*)token;
@@ -133,8 +137,7 @@
 - (void) _logError: (NSString*)msg;
 - (void) _logInfo: (NSString*)msg;
 - (void) _logMessage: (NSString*)msg;
-- (BOOL) _shouldEnableCrashlytics;
-- (NSNumber*) saveAuthCredential: (FIRAuthCredential *) authCredential;
+- (NSNumber*)saveAuthCredential: (FIRAuthCredential *) authCredential;
 - (void)executeGlobalJavascript: (NSString*)jsString;
 
 - (void)createChannel:(CDVInvokedUrlCommand *)command;
@@ -143,12 +146,12 @@
 - (void)listChannels:(CDVInvokedUrlCommand *)command;
 
 @property (nonatomic, copy) NSString *notificationCallbackId;
+@property (nonatomic, copy) NSString *openSettingsCallbackId;
 @property (nonatomic, copy) NSString *tokenRefreshCallbackId;
 @property (nonatomic, copy) NSString *apnsTokenRefreshCallbackId;
-@property (nonatomic, copy) NSString *googleSignInCallbackId;
 @property (nonatomic, copy) NSString *appleSignInCallbackId;
 
 @property (nonatomic, retain) NSMutableArray *notificationStack;
-@property (nonatomic, readwrite) NSMutableDictionary* traces;
+@property(nonatomic, nullable) id<NSObject> installationIDObserver;
 
 @end
